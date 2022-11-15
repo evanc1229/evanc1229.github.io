6ed049656e540ba113bbcce6bd2f47a74ac8456b
christopherdef <christopherdef@gmail.com>
2022-11-14 20:27:02 -0700

    Added navigation

---
+++ js/page2/page2.js
@@ -0,0 +1,13 @@
+import * as utils from "../shared/utils.js";
+
+class Page2 {
+  constructor(data) {
+    console.log("Init Page 2");
+    this.data = data;
+  }
+
+  render() {}
+
+  hide() {}
+}
+export { Page2 };
---
+++ js/page3/page3.js
@@ -0,0 +1,13 @@
+import * as utils from "../shared/utils.js";
+
+class Page3 {
+  constructor(data) {
+    console.log("Init Page 3");
+    this.data = data;
+  }
+
+  render() {}
+
+  hide() {}
+}
+export { Page3 };
