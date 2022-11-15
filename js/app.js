f304ed5a3c849f6ff3cbcb1ca6e9974aa0f5f99b
christopherdef <christopherdef@gmail.com>
2022-11-14 17:18:55 -0700

    Moved TimeSelect instantiation to individual pages Minor cleanup on map event

--- js/app.js
+++ js/app.js
@@ -1,20 +1,19 @@
 import { Page1 } from "./page1/page1.js";
-import {TimeSelect} from "./shared/timeselect.js"
 import { loadData } from "./data.js";
 
 // Initialize each of pages 1, 2, and 3 with dataset
 const data = await loadData();
 
-const ts = new TimeSelect(data)
+// const ts = new TimeSelect(data)
 
 
-const p1 = new Page1(data, ts);
+const p1 = new Page1(data);
 p1.render();
 
 var globalState = {
   data: data,
   p1: p1,
-  ts: ts
+  // ts: ts
 };
 
 // store state globally for debugging
--- js/page1/page1.js
+++ js/page1/page1.js
@@ -1,13 +1,14 @@
 import { MainMap } from "./main_map.js";
+import {TimeSelect} from "../shared/timeselect.js"
 
 // coordinate tooltip.js, main_map.js, timeselect.js
 
 class Page1 {
-  constructor(data, timeselect) {
+  constructor(data) {
     console.log("Init Page 1");
 
     this.data = data;
-    this.timeselect = timeselect;
+    this.timeselect = new TimeSelect(data);
   }
 
   render() {
@@ -21,6 +22,72 @@ class Page1 {
     this.map = new MainMap(mapDiv, this.data);
 
     this.map.render();
+
+    // let dataSlice = this.data.avalanches.slice(0, 5);
+    // let testSvg = d3.select("#test").data(dataSlice);
+
+    // let tool = testSvg
+    //   .append("polygon")
+    //   .attr("id", (d, i) => `tooltip${i}`)
+    //   .attr("height", 600)
+    //   .attr("width", 600)
+    //   .attr(
+    //     "points",
+    //     "318.5 10, 701.5 10, 1010 318.5, 1010 701.5, 701.5 1010, 318.5 1010, 10 701.5, 10 318.5"
+    //   )
+    //   .style("fill", "lightgrey")
+    //   .style("stroke", "black")
+    //   .style("strokeWidth", "10px")
+    //   .style("opacity", 0)
+    //   .style("position", "absolute");
+
+    // testSvg
+    //   .append("circle")
+    //   .attr("x", (d, i) => 100 * i)
+    //   .attr("y", (d, i) => 100 * i)
+    //   .attr("r", 50)
+    //   .attr("fill", "red")
+    //   .on("mouseover", function (e, d) {
+    //     tool.transition().duration(200).style("opacity", 0.9);
+    //     tool
+    //       .html(
+    //         "Attribute  Value   Plot" +
+    //           "<br/>" +
+    //           "Region: " +
+    //           d.Region +
+    //           "" +
+    //           "<br/>" +
+    //           "Place: " +
+    //           d.Place +
+    //           "" +
+    //           "<br/>" +
+    //           "Trigger: " +
+    //           d.Trigger +
+    //           "" +
+    //           "<br/>" +
+    //           "Width: " +
+    //           d.Width +
+    //           "" +
+    //           "<br/>" +
+    //           "Vertical: " +
+    //           d.Vertical +
+    //           "" +
+    //           "<br/>" +
+    //           "Aspect: " +
+    //           d.Aspect +
+    //           "" +
+    //           "<br/>" +
+    //           "Elevation: " +
+    //           d.Elevation +
+    //           "" +
+    //           "<br/>"
+    //       )
+    //       .style("left", d3.select(this).attr("cx") + "px")
+    //       .style("top", d3.select(this).attr("cy") + "px");
+    //   })
+    //   .on("mouseout", function (d) {
+    //     tool.transition().duration(500).style("opacity", 0);
+    //   });
   }
 }
 export { Page1 };
