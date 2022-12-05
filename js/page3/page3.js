7d1128224cc06fb8779312b1d2aeca1b527ba0f0
tannerbenedict <89500218+tannerbenedict@users.noreply.github.com>
2022-12-04 23:27:43 -0700

    Added an about the project page

--- js/page3/page3.js
+++ js/page3/page3.js
@@ -1,4 +1,5 @@
 import * as utils from "../shared/utils.js";
+import {About} from "./about.js";
 import { Page, Component } from "../shared/prototype.js";
 
 class Page3 extends Page {
@@ -6,10 +7,27 @@ class Page3 extends Page {
    *
    * @param {Array<utils.AvalancheData>} data
    */
-  constructor(data, name) {
-    super(data, name);
-    console.log("Init Page 3");
-    this.data = data;
+   constructor(data, name='page2') {
+    console.log("Init Page 2");
+    super(data);
+    this.name = name;
+    this.padding = 10;
+
+    this.config = {
+      about: {
+        width: 1000,
+        height: 1000,
+      },
+    };
+
+    // initialize components
+    this.components = [
+      (this.about = new About(this, this.data, false)),
+    ];
+  }
+
+  async render() {
+    super.render();
   }
 
 }
